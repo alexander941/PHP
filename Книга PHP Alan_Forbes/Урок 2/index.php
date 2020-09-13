dd9119bd6870dc13ca0f93a9f47d53bbb5fade55
Alexander <36586728+alexander941@users.noreply.github.com>
2020-09-13 14:11:09 +0300

    Add files via upload

---
+++ Книга PHP Alan_Forbes/Урок 2/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP Less2</title>
+    </head>
+    <body>
+        <?php 
+            $PriceGas = 25.46;
+            $gallons = 45;
+            $totals = $PriceGas * $gallons;
+            echo "Стоимость полного бака у нас $totals рублей";
+        ?>
+    </body>
+</html>
